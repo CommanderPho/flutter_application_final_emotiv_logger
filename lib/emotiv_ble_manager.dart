771da78abdb5016c0bc18dc8068f68f509eb0d65
Pho Hale <PhoHale@gmail.com>
2025-07-04 14:28:16 -0400

    closer to working again?

--- lib/emotiv_ble_manager.dart
+++ lib/emotiv_ble_manager.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 // import 'package:lsl_flutter/lsl_flutter.dart';
-import 'package:liblsl/lsl.dart';
-import 'package:liblsl/native_liblsl.dart';
 import 'crypto_utils.dart';
 
 class EmotivBLEManager {
