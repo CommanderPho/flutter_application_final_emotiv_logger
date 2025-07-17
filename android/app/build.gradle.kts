140cbc205e697491dc34b028e617cbcac3ee61d8
Pho Hale <PhoHale@gmail.com>
2025-07-17 05:20:53 -0400

    renamed from "flutter_application_final_emotiv_logger" to "flutter_emotiv_logger"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_application_final_emotiv_logger"
+    namespace = "com.PhoHale.flutter_emotiv_logger"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973" // flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_application_final_emotiv_logger"
+        applicationId = "com.PhoHale.flutter_emotiv_logger"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 27
--- android/app/src/main/kotlin/com/PhoHale/flutter_emotiv_logger/MainActivity.kt
+++ android/app/src/main/kotlin/com/PhoHale/flutter_emotiv_logger/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_application_final_emotiv_logger
+package com.PhoHale.flutter_emotiv_logger
 
 import io.flutter.embedding.android.FlutterActivity
 
